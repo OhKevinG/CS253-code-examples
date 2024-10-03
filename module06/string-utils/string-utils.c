cdffccd0c4fc1d6a05a49a0fbe59b54da0595295
OhKevinG <kevingutierrez@u.boisestate.edu>
2024-10-03 14:15:59 -0600

    updated strings-utils.c

--- module06/string-utils/string-utils.c
+++ module06/string-utils/string-utils.c
@@ -17,16 +17,45 @@ char *mystrncpy(char *dest, char *src, int maxlen) {
 
 /* mystrnlen - determine the length of a fixed-size string */
 int mystrnlen(char *s, int maxlen) {
-    return -1;
+    int count = 0;
+    int i;
+
+    for(i = 0; i < maxlen && s[i] != '\0'; ++i) {
+        count++;
+    }
+    return count;
 }
 
 /* mystrcat - concatenate two strings */
 char *mystrcat(char *dest, char *src) {
+    //char *newString;
+    int i = mystrnlen(dest, MAX_LEN);
+    int j = 0;
+
+    //mystrncpy(newString, dest, MAX_LEN);
+
+    while (src[j] != '\0') {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    
+    dest[i] = '\0';
+
     return dest;
 }
 
 /* mystrrev - reverse a string */
 void strrev(char *s){
+
+    int i = mystrnlen(s, MAX_LEN);
+    int j;
+
+    for (j = 0; j < i; ++j, --i) {
+        char temp = s[j];
+        s[j] = s[i - 1];
+        s[i - 1] = temp;
+    }
     
 }
 
